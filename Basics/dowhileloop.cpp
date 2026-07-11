6d0ad3abd6eb8361e215b887cc1d60fbc5e54a56
Siddhi Mali <siddhimali2511@gmail.com>
2026-07-11 18:44:45 +0530

    wile and do while loops

---
+++ Basics/dowhileloop.cpp
@@ -0,0 +1,16 @@
+// WHEN YOU WANT THE LOOP TO EXECUTE A MNIMUM OF ONE TIME, WO PLACETHE DO WHILE LOOP. SO UNLIKE IN WHILE IF THE CONDITION IS FALCE, THE LOO JUST NEVER RUNS, IN DO WHILE THE LOOP WILL RUN ONCE AND THEN CHECK TH CONDITION.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int i = 15;
+    do
+    {
+        cout << "Siddhi" << "\n";
+        i++;
+    } while (i <= 10);
+
+    return 0;
+}
---
+++ Basics/whileloop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int i = 1;
+    while (i <= 10)
+    {
+        cout << "Siddhi" << "\n";
+        i++;
+    }
+
+    return 0;
+}
