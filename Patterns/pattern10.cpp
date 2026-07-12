c4620d52ddcacc5f5e3e51e61dc39cc0d83210ca
Siddhi Mali <siddhimali2511@gmail.com>
2026-07-12 22:29:01 +0530

    pattern 10 another way added

--- Patterns/pattern10.cpp
+++ Patterns/pattern10.cpp
@@ -48,3 +48,33 @@ int main()
 
     return 0;
 }
+
+// ANOTHER WAY
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void pattern(int n)
+// {
+//     for (int i = 1; i <= 2 * n - 1; i++)
+//     {
+//         int s = i;
+//         if (i > n) s = (2 * n) - i;
+//         for (int j = 1; j <= s; j++)
+//         {
+//             cout << "*";
+//         }
+//         cout << "\n";
+//     }
+// }
+
+// int main()
+// {
+
+//     int a;
+//     cout << "enter the no.: ";
+//     cin >> a;
+//     pattern(a);
+
+//     return 0;
+// }
